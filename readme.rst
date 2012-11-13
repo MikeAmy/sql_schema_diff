99d82f86a6f74046aaa12110c73858cb5182c83d
Mike Amy <cocoade@googlemail.com>
2012-11-13 19:39:26 +0700

    Added setup.py and readme.rst

---
+++ readme.rst
@@ -0,0 +1,12 @@
+Python SQL Schema Parsing / Introspection / Diffing
+--------------------------------------------------
+
+Parser and diff tool. It can be used to:
+
+Parse schemas (sqlite, postgresql)
+Introspect schemas (postgresql).
+Diff schemas.
+
+E.g. this can be used to compare migrated and unmigrated schemas.
+
+This is a work in progress.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import os
+
+setup(
+    name='python_sql_schema_comparison',
+    version='1.0',
+    description='Python SQL Schema Parsing and Diffing',
+    author='Mike Amy',
+    author_email='cocoade@googlemail.com',
+    url='https://github.com/MikeAmy/python_sql_schema_comparison',
+    packages=['sql_schema_diff',],
+    keywords=['SQL', 'database', 'schema', 'postgres', 'sqlite', 'diff'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst'),
+    ).read().strip(),
+)
