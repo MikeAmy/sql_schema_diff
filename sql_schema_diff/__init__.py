7606fa816944aec36a3b794254ea270ae0e710ff
Mike Amy <cocoade@googlemail.com>
2012-11-13 21:25:02 +0700

    Fixed imports

--- sql_schema_diff/__init__.py
+++ sql_schema_diff/__init__.py
@@ -1,5 +1,3 @@
-from sql_schema_diff.schema import  Table, Column, Index
-
+from sql_schema_diff.schema import Schema, Table, Column, Index
 from sql_schema_diff import diff, parse, introspect
-from sql_schema_diff.schema import Schema
 
--- sql_schema_diff/schema/__init__.py
+++ sql_schema_diff/schema/__init__.py
@@ -1,1 +1,6 @@
-from sql_schema_diff.schema import Index, reference_resolution, Schema, Table, Column
+from sql_schema_diff.schema.Schema import Schema
+from sql_schema_diff.schema.Table import Table
+from sql_schema_diff.schema.Column import Column
+from sql_schema_diff.schema.Index import Index
+
+import reference_resolution
